f9c351ae74562880f46fa99360ee892525217ced
Thomas Ubensee <thomasu@motorola.com>
2018-01-27 15:55:31 -0200

    Initial Commit

---
+++ app/src/main/java/itsallcode/org/fasttraceviewerforandroid/repository/FastTraceRepository.java
@@ -0,0 +1,13 @@
+package itsallcode.org.fasttraceviewerforandroid.repository;
+
+import java.util.List;
+
+//import openfasttrack.core.SpecificationItem;
+
+/**
+ * Created by thomasu on 1/27/18.
+ */
+
+public interface FastTraceRepository {
+//    void add(final String name, final List<SpecificationItem> itemList);
+}
